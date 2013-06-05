f1a26903793411e6abdef292e235f369b0dfd378
nd0ut <nd0ut.me@gmail.com>
2013-06-05 22:30:19 +0400

    move addr2maps helper to application_helper some refactor of current_page helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,9 @@
 module ApplicationHelper
   def cp(path)
-    "active" if current_page?(path)
+    "active" if current_page?(path) || current_page?(root_path)
+  end
+
+  def addr2maps(addr, city)
+    return "http://maps.google.com/?q=#{addr.tr ' ', '+'}+#{city.tr ' ', '+'}+Евразия"
   end
 end
