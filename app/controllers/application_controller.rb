c1abc055e62a53a8c6ea10927bf58056b7a4e467
nd0ut <nd0ut.me@gmail.com>
2013-06-06 12:20:01 +0400

    404 page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  rescue_from ActiveRecord::RecordNotFound, :with => :render_404
+
+end
+
+def render_404
+  render :template => 'error_pages/404', :layout => true, :status => :not_found
 end
