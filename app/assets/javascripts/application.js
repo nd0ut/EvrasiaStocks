1a59cc9c3d6643ab780b33df0276e40c419a4a04
nd0ut <nd0ut.me@gmail.com>
2013-06-07 12:19:22 +0400

    jquery.highlight moved to vendor

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,7 @@
 // GO AFTER THE REQUIRES BELOW.
 //
 //= require jquery
+//= require jquery.highlight
 // require jquery_ujs
 //= require foundation/foundation
 //= require foundation/foundation.topbar
