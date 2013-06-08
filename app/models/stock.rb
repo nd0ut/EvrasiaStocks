bd8ade0b2c0b84072033d068161546594992c1dc
nd0ut <nd0ut.me@gmail.com>
2013-06-08 15:44:45 +0400

    рефакторинг

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -2,7 +2,7 @@ class Stock < ActiveRecord::Base
   has_many :stock_hours
   has_many :restaurants, :through => :stock_hours
 
-  def originalLink
+  def original_link
     return "http://evrasia.spb.ru/public/akcii/view/#{id}.html"
   end
 end
