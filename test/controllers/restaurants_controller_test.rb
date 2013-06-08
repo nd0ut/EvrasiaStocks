090a0df44b4e0e2776b59b3b0bc3c1dec98461e1
nd0ut <nd0ut.me@gmail.com>
2013-06-08 12:25:30 +0400

    написал тесты для контроллеров

--- test/controllers/restaurants_controller_test.rb
+++ test/controllers/restaurants_controller_test.rb
@@ -1,13 +1,18 @@
 require 'test_helper'
 
 class RestaurantsControllerTest < ActionController::TestCase
+  setup do
+    @restaurant = restaurants(:restaurant1)
+  end
+
   test "should get index" do
     get :index
     assert_response :success
+    assert_not_nil assigns(:restaurants)
   end
 
   test "should get show" do
-    get :show
+    get :show, id: @restaurant
     assert_response :success
   end
 
--- test/controllers/stocks_controller_test.rb
+++ test/controllers/stocks_controller_test.rb
@@ -1,13 +1,19 @@
 require 'test_helper'
 
 class StocksControllerTest < ActionController::TestCase
+  setup do
+    @stock = stocks(:stock1)
+  end
+
   test "should get index" do
     get :index
     assert_response :success
+    assert_not_nil assigns(:stocks)
   end
 
   test "should get show" do
-    get :show
+    get :show, id: @stock
     assert_response :success
   end
+
 end
