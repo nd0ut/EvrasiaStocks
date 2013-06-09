85ac744cdd853bfeb432a0ea9b19b13c0f37bab9
nd0ut <nd0ut.me@gmail.com>
2013-06-10 01:42:04 +0400

    сортировка ресторанов по статусу акции (сначала те, что сейчас)

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -11,6 +11,13 @@ class StocksController < ApplicationController
   def show
     @stock = Stock.includes(:restaurants => :city).find(params[:id])
 
+    @ordered_restaurants = @stock.restaurants.sort do |a, b|
+      # TODO: блять, тут пиздец как неоптимизированно, но я не знаю как иначе :(
+      a_now = @stock.stock_hours.find_by_restaurant_id(a.id).now?
+
+      a_now ? -1 : 1
+    end
+
     respond_to do |format|
       format.html
       format.json
